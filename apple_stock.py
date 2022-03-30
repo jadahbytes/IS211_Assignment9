09f3453f081355531a567eb9d18ba24fad1fc04c
jadahbytes <jadahbytes@gmail.com>
2022-03-30 19:45:12 -0400

    Completed assignment

--- apple_stock.py
+++ apple_stock.py
@@ -1,2 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
+
+hdrs = {"authority": "finance.yahoo.com",
+            "method": "GET",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64)"}
+
+
+def main(url):
+    response = requests.get(url, headers = hdrs)
+    soup = BeautifulSoup(response.text, features="lxml")
+
+    table = soup.find('table', attrs={'class': 'W(100%) M(0)'})
+    rows = table.find_all('tr', attrs={'class': 'BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)'})
+    for row in rows:
+        data = row.find_all('td')
+        try:
+            date = data[0].get_text()
+            close = data[4].get_text()
+        except:
+            continue
+        print(f'''
+        +-------------------+
+        |Date: {date}
+        |Closing Price: ${close}
+        +-------------------+
+        ''')
+
 if __name__ == "__main__":
-    pass
+    main(url)
--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+
+url = 'https://www.cbssports.com/nfl/stats/player/passing/nfl/regular/qualifiers/?sortcol=td&sortdir=descending'
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html5lib')
+#print(soup.prettify())
+#stats = soup.find_all('tr', attrs={'class': 'TableBase-bodyTr'})
+#print(stats)
+
+
+def main(url):
+    for tr in soup.find_all('tr', class_='TableBase-bodyTr', limit=20):
+        #print(tr)
+        name = tr.find('span', class_='CellPlayerName--long').a.text.strip()
+        #print("Name: ", name)
+        position = tr.find('span', class_='CellPlayerName-position').text.strip()
+        #print("Postion: ", position)
+        team = tr.find('span', class_='CellPlayerName-team').text.strip()
+        #print("Team: ", team)
+        find_tds = tr.find_all('td')
+        touchdowns = find_tds[8].get_text().strip()
+        #print("Touchdowns", touchdowns)
+
+        print(f'''
+        +-------------------+
+        |Name: {name}
+        |Position: {position}
+        |Team: {team}
+        |Touchdowns: {touchdowns}
+        +-------------------+
+        ''')
+
 if __name__ == "__main__":
-    pass
+    main(url)
